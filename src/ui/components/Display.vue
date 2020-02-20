d646cbab8c230196aa1fbcba0dd96b9a4fd38116
6160 <marco@sixonesixo.com>
2020-02-20 14:59:52 +0100

    changed input to text-area and added micro guide

--- src/ui/components/Display.vue
+++ src/ui/components/Display.vue
@@ -3,7 +3,7 @@
       <b-breadcrumb  v-on:click="previous" :items="$store.getters.listHeader"></b-breadcrumb>
     <!-- <div v-on:click="previous">{{ $store.getters.listHeader}}</div> -->
     <div id="container" style="display: inline-flex;width: 100%; ">
-      <div id="list" style="height: 700px; width: 200px;overflow: auto;">
+      <div id="list" style="height: 700px; width: 200px;overflow: auto; font-size: 12px">
         <b-list-group name='LIST'>
           <b-list-group-item
             v-on:click="loadList"
@@ -14,17 +14,38 @@
           >{{item.name}}</b-list-group-item>
         </b-list-group>
       </div>
-      <div id="text" class="w-100 p-3 bg-secondary text-light" style="height: 700px;overflow: auto">
+      <div v-if="$store.getters.texts.length" id="text" class="w-100 p-3 bg-secondary text-light" style="height: 700px;overflow: auto">
         <ol>
           <li
             v-for="(item, index) in $store.getters.texts"
             v-bind:key="index"
             v-bind:id="item.id"
             v-bind:name="item.name"
+            style="padding-bottom: 3px;"
           >
-              <b-form-input  v-on:keyup.enter="changed" name="text" v-bind:value="item.text" v-bind:id="item.id"/>
+              <b-form-textarea size="sm" rows="3" max-rows="6" v-on:keyup.ctrl.enter="changed" name="text" v-bind:value="item.text" v-bind:id="item.id"/>
           </li>
         </ol>
+      </div>
+      <div v-else class="w-100 p-3 bg-secondary text-dark" style="height: 700px;overflow: auto"> 
+        <b-card
+          title="HELLO!"
+          img-src="https://i.imgur.com/0aVRBYA.png"
+          img-alt="Image"
+          img-top
+          tag="article"
+          style="max-width: 25rem;"
+          class="mb-2"
+        >
+          <b-card-text style="font-size: 12px">
+            Just a quick introduction: <br/> <br/>
+           <il>
+             <li> Navigate pages and artboards with the list on the left.</li>
+            <li>Edit the text in the text area of selected artboard and then save to Figma with <b>ctrl+enter</b>.</li>
+            </il>
+          </b-card-text>
+
+        </b-card>
       </div>
     </div>
     <div> {{$store.getters.lastChange}}</div>
